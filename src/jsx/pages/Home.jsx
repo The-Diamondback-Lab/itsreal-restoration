3b20f6a00386a75d4abbec8a059472720cbdba42
Vishal Patel <vishalpatel3199@gmail.com>
2021-03-16 22:09:26 -0400

    Fixed issue of root being a centered element
    Page-specific styles (i.e. vertically and horizontally centering all content) are no longer applied directly to root. Instead, more containers are provided to isolate this kind of behavior

--- src/jsx/pages/Home.jsx
+++ src/jsx/pages/Home.jsx
@@ -7,15 +7,17 @@ import specialProjectBanner from '../../assets/special-project-banner.png';
 
 export default function Home() {
   return (
-    <div id="home-container">
-      <img className="special-projects-banner" src={specialProjectBanner} alt="Special Project Logo"></img>
-      <div id="home-content">
-        <img className="mobile-banner" src={mobileBanner} alt="&quot;It's Real&quot; Banner"></img>
-        {/* lsquo and rsquo are "fancy" single quotes */}
-        <h1>&lsquo;It&rsquo;s real&rsquo;</h1>
-        <p>A seven-part look at how sexual assault is handled at the University
-          of Maryland.</p>
-        <NavigationElements />
+    <div id="home-root">
+      <div id="home-container">
+        <img className="special-projects-banner" src={specialProjectBanner} alt="Special Project Logo"></img>
+        <div id="home-content">
+          <img className="mobile-banner" src={mobileBanner} alt="&quot;It's Real&quot; Banner"></img>
+          {/* lsquo and rsquo are "fancy" single quotes */}
+          <h1>&lsquo;It&rsquo;s real&rsquo;</h1>
+          <p>A seven-part look at how sexual assault is handled at the University
+            of Maryland.</p>
+          <NavigationElements />
+        </div>
       </div>
     </div>
   );

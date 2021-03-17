4c343e1530dbf0c0bca5c710316fc1cb53924703
Vishal Patel <vishalpatel3199@gmail.com>
2021-03-16 22:05:40 -0400

    Better css imports

--- src/jsx/pages/Home.jsx
+++ src/jsx/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import '../../styles/home.scss';
 import NavigationElements from '../components/NavigationElements';
 
 import mobileBanner from '../../assets/mobile-banner.png';

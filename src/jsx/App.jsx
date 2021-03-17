e2c4782ead2acaef95143ffd8a04d7e0c30b502a
Vishal Patel <vishalpatel3199@gmail.com>
2021-03-16 21:39:52 -0400

    Added routes for more pages
    Also added placeholder files

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -4,10 +4,21 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Test from "./pages/Test";
+import Article from './pages/Article';
+
+import tocData from '../data/toc.json';
 
 export default function App() {
+  // Mapping each part in table of contents to a Route object
+  let articleRoutes = tocData.map((x, i) => (
+    <Route exact path={'/' + x.path} key={`route-part-${i+1}`}>
+      <Article part={x.path} hasTimeline={x.hasTimeline}/>
+    </Route>
+  ));
+
   return (
     <Router>
         <Switch>
@@ -17,6 +28,7 @@ export default function App() {
           <Route exact path="/test">
             <Test />
           </Route>
+          {articleRoutes}
         </Switch>
     </Router>
   );

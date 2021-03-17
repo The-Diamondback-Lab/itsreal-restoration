468ccbda7f09bc2cc3d68d592be84d3965ff0e5a
Vishal Patel <vishalpatel3199@gmail.com>
2021-03-16 22:06:35 -0400

    Comments

--- src/jsx/pages/Home.jsx
+++ src/jsx/pages/Home.jsx
@@ -11,6 +11,7 @@ export default function Home() {
       <img className="special-projects-banner" src={specialProjectBanner} alt="Special Project Logo"></img>
       <div id="home-content">
         <img className="mobile-banner" src={mobileBanner} alt="&quot;It's Real&quot; Banner"></img>
+        {/* lsquo and rsquo are "fancy" single quotes */}
         <h1>&lsquo;It&rsquo;s real&rsquo;</h1>
         <p>A seven-part look at how sexual assault is handled at the University
           of Maryland.</p>

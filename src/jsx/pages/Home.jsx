22c7acf3bcc1515093e042f3baaf39744fff4bb2
Vishal Patel <vishalpatel3199@gmail.com>
2021-03-16 20:31:37 -0400

    Centered special projects banner

--- src/jsx/pages/Home.jsx
+++ src/jsx/pages/Home.jsx
@@ -4,10 +4,12 @@ import '../../styles/home.scss';
 import NavigationElements from '../components/NavigationElements';
 
 import mobileBanner from '../../assets/mobile-banner.png';
+import specialProjectBanner from '../../assets/special-project-banner.png';
 
 function Home() {
   return (
     <div id="home-container">
+      <img className="special-projects-banner" src={specialProjectBanner} alt="Special Project Logo"></img>
       <div id="home-content">
         <img className="mobile-banner" src={mobileBanner} alt="&quot;It's Real&quot; Banner"></img>
         <h1>&lsquo;It&rsquo;s real&rsquo;</h1>

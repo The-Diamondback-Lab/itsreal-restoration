168af8072fa0b6af2eab2d160924effc87f92fcb
Vishal Patel <vishalpatel3199@gmail.com>
2021-03-16 21:09:15 -0400

    Added toc.json
    "table of contents" file allows us to consistently use this data anywhere in the app

--- src/jsx/components/NavigationElements.jsx
+++ src/jsx/components/NavigationElements.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import tocData from '../../data/toc.json';
+
 /**
  * Gives a list of hyperlinked elements for navigating to different pages of
  * the project. The list of elements **are hardcoded**; however the returning
@@ -10,21 +12,15 @@ import React from 'react';
  * @returns a `div` element whose children are anchor tags `a`
  */
 export default function NavigationElements() {
-  // Text used for the anchor tags
-  let anchorText = [ 'I.', 'II.', 'III.', 'IV.', 'V.', 'VI.', 'VII.' ];
-  // Links for the anchor tags (this should be a parallel-array with anchorText)
-  let links = [
-    'on-her-own', 'inside-the-culture', 'care-granted', 'i-can-never-escape',
-    'work-in-progress', 'insufficient-evidence', 'first-line-of-defense'
-  ];
+  console.log(tocData);
 
   return (
     <div id="navigation-elems">
-      {links.map((link, i) =>
-        <a href={'/' + link}
+      {tocData.map((data, i) =>
+        <a href={'/' + data.path}
           key={`nav-part-${i}`}
           target="_blank"
-          rel="noreferrer">{anchorText[i]}</a>
+          rel="noreferrer">{data.numeral}</a>
       )}
     </div>
   );

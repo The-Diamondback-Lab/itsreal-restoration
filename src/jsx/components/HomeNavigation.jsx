0b8b530838737ea255631da648efa491bfab812f
Vishal Patel <vishalpatel3199@gmail.com>
2021-04-17 14:06:31 -0400

    cleanup

--- src/jsx/components/HomeNavigation.jsx
+++ src/jsx/components/HomeNavigation.jsx
@@ -25,16 +25,18 @@ export default class HomeNavigation extends React.Component {
   }
 
   render() {
+    let links = tocData.map((data, i) =>
+      <a href={'/' + data.path}
+        className="transition"
+        onMouseEnter={this.onMouseEnter.bind(this, i+1)}
+        onMouseLeave={this.onMouseLeave.bind(this, i+1)}
+        rel="noreferrer"
+        key={`nav-part-${i+1}`}>{data.numeral}</a>
+    );
+
     return (
       <div id="navigation-elems">
-        {tocData.map((data, i) =>
-          <a href={'/' + data.path}
-            className="transition"
-            onMouseEnter={this.onMouseEnter.bind(this, i+1)}
-            onMouseLeave={this.onMouseLeave.bind(this, i+1)}
-            rel="noreferrer"
-            key={`nav-part-${i+1}`}>{data.numeral}</a>
-        )}
+        {links}
       </div>
     );
   }

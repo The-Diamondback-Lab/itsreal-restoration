dfbd01dd78331a17197c5acd184526b0edd94234
Vishal Patel <vishalpatel3199@gmail.com>
2021-04-21 22:37:36 -0400

    Added roman numeral text and links to short nav

--- src/jsx/components/ShortNavigation.jsx
+++ src/jsx/components/ShortNavigation.jsx
@@ -1,25 +1,29 @@
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+const romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+
 /**
  * @param {ShortNavigationPropTypes} props
  */
 export default function ShortNavigation(props) {
+  const { pageNumber, prevPagePath, nextPagePath } = props;
+
   let prevNav = null;
   let nextNav = null;
 
-  if (props.prevPagePath) {
+  if (prevPagePath) {
     prevNav = (
       <div>
         <FiChevronLeft size="4em" />
-        <a>{props.pageNumber-1}</a>
+        <a href={'/' + prevPagePath}>{romanNumerals[pageNumber-1]}</a>
       </div>
     );
   }
 
-  if (props.nextPagePath) {
+  if (nextPagePath) {
     nextNav = (
       <div>
-        <a>{props.pageNumber+1}</a>
+        <a href={'/' + nextPagePath}>{romanNumerals[pageNumber+1]}</a>
         <FiChevronRight size="4em" />
       </div>
     );

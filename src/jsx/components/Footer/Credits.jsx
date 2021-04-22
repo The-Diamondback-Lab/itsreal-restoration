35a3a596d79011b48b4b28f0c7cd6b2390b7f8d1
Vishal Patel <vishalpatel3199@gmail.com>
2021-04-22 00:37:46 -0400

    Added credit layout

---
+++ src/jsx/components/Footer/Credits.jsx
@@ -0,0 +1,39 @@
+export default function Credits() {
+  return (
+    <div className="credits">
+      <h1>Credits</h1>
+      <div className="sub-credit">
+        <p>Reporters</p>
+        <p>Hallie Miller, Ellie Silverman, Danielle Ohl, Talia Richman, Taylor Swaak, Mina Haq, Lexie Schapitl and Natalie Schwartz</p>
+      </div>
+      <div className="split">
+        <div className="sub-credit">
+          <p>Design &amp; Development</p>
+          <p>Jake Hughes</p>
+        </div>
+        <div className="sub-credit">
+          <p>Project Editor</p>
+          <p>Ellie Silverman</p>
+        </div>
+      </div>
+      <div className="split">
+        <div className="sub-credit">
+          <p>Sketch Artist</p>
+          <p>Yvette Yu</p>
+        </div>
+        <div className="sub-credit">
+          <p>Video Editor</p>
+          <p>Hallie Miller</p>
+        </div>
+      </div>
+      <div className="sub-credit">
+        <p>Photo Editor</p>
+        <p>Tom Hausman</p>
+      </div>
+      <div className="sub-credit">
+        <p>Copy Editors</p>
+        <p>Ryan Romano, Uyen Nguyen, Joe Ilardi, Casey Noenickx and Rebecca Stryer</p>
+      </div>
+    </div>
+  );
+}
--- src/jsx/components/Footer/index.jsx
+++ src/jsx/components/Footer/index.jsx
@@ -1,9 +1,12 @@
 import About from "./About";
+import Credits from "./Credits";
 
 export default function Footer() {
   return (
     <div className="footer">
       <About />
+      <div className="divider" />
+      <Credits />
     </div>
   );
 }

c0992f78602618fbd0055efc4d39ec5bddbbc573
Vishal Patel <vishalpatel3199@gmail.com>
2021-05-01 15:33:28 -0400

    Byline has hyperlink

--- src/jsx/components/Article/ArticleContent.jsx
+++ src/jsx/components/Article/ArticleContent.jsx
@@ -7,7 +7,7 @@ export default function ArticleContent(props) {
   return (
     <div id="article-container">
       <h1>{props.title}</h1>
-        <p className="byline">By John Doe</p>
+        <p className="byline">By <a href="https://TODO.com">John Doe</a></p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum aspernatur officiis obcaecati labore molestiae ratione atque, optio sequi cum omnis quis, facilis eaque? Id vitae quam officiis repellat voluptatum quis.</p>
     </div>
   )

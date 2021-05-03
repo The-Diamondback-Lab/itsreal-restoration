eec153d3c90493aba87db9d7be3e3112758b0a29
Vishal Patel <vishalpatel3199@gmail.com>
2021-05-03 19:54:48 -0400

    Removed unused vars

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,6 @@ import authorLinks from './data/author-links.json';
 
 const DEFAULT_HEADER = "‘It’s real’";
 const DEFAULT_BLURB = "A seven-part look at how sexual assault is handled at the University of Maryland.";
-const DEFAULT_BACKGROUND = "/assets/watercolor-black-bg.jpg";
 
 /**
  * @type {Object<string, string>}
@@ -17,7 +16,6 @@ export const headers = { DEFAULT: DEFAULT_HEADER };
  * @type {Object<string, string>}
  */
 export const blurbs = { DEFAULT: DEFAULT_BLURB };
-export const backgrounds = { DEFAULT: DEFAULT_BACKGROUND };
 
 tocData.forEach((x, i) => {
   let header = x.path.replace(/-/gi, ' ');
@@ -26,7 +24,6 @@ tocData.forEach((x, i) => {
   let key = `part-${i+1}`;
   headers[key] = header;
   blurbs[key] = blurb;
-  backgrounds[key] = `/assets/part${i+1}-bg.jpg`;
 });
 
 export { tocData };
